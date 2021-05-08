e7d2a94e9f4c459c501b4883d2a8ac120761c14b
Pratham Choudhary <45418811+pratham27037@users.noreply.github.com>
2021-05-08 17:54:54 +0530

    first commit

---
+++ src/Row.js
@@ -0,0 +1,18 @@
+import React,{useState} from 'react'
+
+function Row({title}) {
+
+    const [movies,setMovies] = useState([]);
+    
+
+    return (
+        <div>
+            {/**title */}
+            <h2>{title}</h2>
+
+            {/**container->poster */}
+        </div>
+    )
+}
+
+export default Row;
---
+++ src/requests.js
@@ -0,0 +1,14 @@
+const APIKEY="06fde19d4d12d485f0d34897990e86ea";
+
+const requests = {
+    fetchTrending: `/trending/all/week?api_key=${APIKEY}&language=en-US`,
+    fetchNetflixOriginals:`/discover/tv?api_key=${APIKEY}&with_networks=213`,
+    fetchTopRated:`/movie/top_rated?api_key=${APIKEY}&language=en-US`,
+    fetchActionMovies:`/discover/movie?api_key=${APIKEY}&with_genres=28`,
+    fetchComedyMovies:`/discover/movie?api_key=${APIKEY}&with_genres=35`,
+    fetchHorrorMovies:`/discover/movie?api_key=${APIKEY}&with_genres=27`,
+    fetchRomanceMovies:`/discover/movie?api_key=${APIKEY}&with_genres=10749`,
+    fetchDocumentariesMovies:`/discover/movie?api_key=${APIKEY}&with_genres=99`,
+}
+
+export default requests;
